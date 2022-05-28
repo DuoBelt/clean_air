7a780677e702f9cf1a99beecf5b25518e5f14445
DuoBelt <mariuszwziatek@gmail.com>
2022-05-28 08:16:08 +0200

    8.1. Ramowy rozkład elementów na ekranie pogody

--- lib/WeatherScreen.dart
+++ lib/WeatherScreen.dart
@@ -12,68 +12,72 @@ class WeatherScreen extends StatefulWidget {
 class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                color: new Color(0xffffffff),
-                gradient: LinearGradient(
-                    begin: Alignment.centerRight,
-                    end: Alignment.centerLeft,
-                    colors: [new Color(0xfff6e6cd8), new Color(0xff77e1ee)])),
-          ),
-          Align(
-              alignment: FractionalOffset.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image(
-                    image: AssetImage('icons/cloud-sun.png'),
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 15.0)),
-                  Text(Strings.appTitle,
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.lato(
-                          textStyle: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 42.0,
-                        color: Colors.white,
-                      ))),
-                  Padding(padding: EdgeInsets.only(top: 6.0)),
-                  Text('Pogoda \n ',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.lato(
-                          textStyle: TextStyle(
-                        fontSize: 18.0,
-                        color: Colors.white,
-                      ))),
-                ],
-              )),
-          Positioned(
-              left: 0,
-              bottom: 35,
-              right: 0,
-              child: Container(
-                alignment: Alignment.center,
-                child: Text("Przywiewam dane ...",
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.lato(
-                        textStyle: TextStyle(
-                      fontWeight: FontWeight.w300,
-                      fontSize: 18.0,
-                      color: Colors.white,
-                    ))),
-              ))
-        ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          fit: StackFit.expand,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  color: new Color(0xffffffff),
+                  gradient: LinearGradient(
+                      begin: Alignment.centerRight,
+                      end: Alignment.centerLeft,
+                      colors: [new Color(0xfff6e6cd8), new Color(0xff77e1ee)])),
+            ),
+            Align(
+                alignment: FractionalOffset.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(padding: EdgeInsets.only(top: 45.0)),
+
+                    Image(
+                      image: AssetImage('icons/weather-sunny.png'),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 41.0)),
+                    Text("Poniedziałek 31.05, 21:00 Słonecznie"),
+                    Padding(padding: EdgeInsets.only(top: 12.0)),
+                    Text("14°C"),
+                    Text("Odczuwalna 13°C"),
+                    IntrinsicHeight(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 130,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text("Cisnienie"),
+                                  Padding(padding: EdgeInsets.only(top: 2.0)),
+                                  Text("1020 hPa")
+                                ],
+                              ),
+                            ),
+                            VerticalDivider(
+                              width: 48,
+                              thickness: 1,
+                            ),
+                            Container(
+                              width: 130,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text("Wiatr"),
+                                  Padding(padding: EdgeInsets.only(top: 2.0)),
+                                  Text("16 k/h ")
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 24.0)),
+                    Text("Opady 0,1 mm/12h"),
+                    Padding(padding: EdgeInsets.only(top: 68.0)),
+                  ],
+                )),
+          ]), // This trailing comma makes auto-formatting nicer for build methods.
     );
-
-  }
-
-  bool havePermission() {
-    return true;
   }
 }
