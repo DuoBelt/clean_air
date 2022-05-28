43492561c328b45a676353cee39e629f3ab4ad4a
DuoBelt <mariuszwziatek@gmail.com>
2022-05-28 08:42:00 +0200

    8.2. Stylowanie ekranu pogody

--- lib/WeatherScreen.dart
+++ lib/WeatherScreen.dart
@@ -13,71 +13,130 @@ class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                  color: new Color(0xffffffff),
-                  gradient: LinearGradient(
-                      begin: Alignment.centerRight,
-                      end: Alignment.centerLeft,
-                      colors: [new Color(0xfff6e6cd8), new Color(0xff77e1ee)])),
-            ),
-            Align(
-                alignment: FractionalOffset.center,
-                child: Column(
+      body: Stack(fit: StackFit.expand, children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+              color: new Color(0xffffffff),
+              gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  colors: [
+                    new Color(0xff6E6CD8),
+                    new Color(0xff40A0EF),
+                    new Color(0xff77E1EE),
+                  ])),
+        ),
+        Align(
+            alignment: FractionalOffset.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(padding: EdgeInsets.only(top: 45.0)),
+                Image(
+                  image: AssetImage('icons/weather-sunny.png'),
+                ),
+                Padding(padding: EdgeInsets.only(top: 41.0)),
+                Text("Poniedziałek 31.05, 21:00 Słonecznie",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.lato(
+                      textStyle: TextStyle(
+                          fontSize: 14.0,
+                          height: 1.2,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400)),
+                ),
+                Padding(padding: EdgeInsets.only(top: 12.0)),
+                Text("14°C",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.lato(
+                      textStyle: TextStyle(
+                          fontSize: 64.0,
+                          height: 1.2,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700)),),
+                Text("Odczuwalna 13°C",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.lato(
+                      textStyle: TextStyle(
+                          fontSize: 14.0,
+                          height: 1.2,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700)),),
+                Padding(padding: EdgeInsets.only(top: 25.0)),
+                IntrinsicHeight(
+                    child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(padding: EdgeInsets.only(top: 45.0)),
-
-                    Image(
-                      image: AssetImage('icons/weather-sunny.png'),
+                  children: [
+                    Container(
+                      width: 130,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text("Cisnienie",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.lato(
+                                textStyle: TextStyle(
+                                    fontSize: 14.0,
+                                    height: 1.2,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300)),),
+                          Padding(padding: EdgeInsets.only(top: 2.0)),
+                          Text("1020 hPa",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.lato(
+                                textStyle: TextStyle(
+                                    fontSize: 26.0,
+                                    height: 1.2,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700)),)
+                        ],
+                      ),
                     ),
-                    Padding(padding: EdgeInsets.only(top: 41.0)),
-                    Text("Poniedziałek 31.05, 21:00 Słonecznie"),
-                    Padding(padding: EdgeInsets.only(top: 12.0)),
-                    Text("14°C"),
-                    Text("Odczuwalna 13°C"),
-                    IntrinsicHeight(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: 130,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Text("Cisnienie"),
-                                  Padding(padding: EdgeInsets.only(top: 2.0)),
-                                  Text("1020 hPa")
-                                ],
-                              ),
-                            ),
-                            VerticalDivider(
-                              width: 48,
-                              thickness: 1,
-                            ),
-                            Container(
-                              width: 130,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text("Wiatr"),
-                                  Padding(padding: EdgeInsets.only(top: 2.0)),
-                                  Text("16 k/h ")
-                                ],
-                              ),
-                            ),
-                          ],
-                        )
+                    VerticalDivider(
+                      width: 48,
+                      color: Colors.white,
+                      thickness: 1,
                     ),
-                    Padding(padding: EdgeInsets.only(top: 24.0)),
-                    Text("Opady 0,1 mm/12h"),
-                    Padding(padding: EdgeInsets.only(top: 68.0)),
+                    Container(
+                      width: 130,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("Wiatr",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.lato(
+                                textStyle: TextStyle(
+                                    fontSize: 14.0,
+                                    height: 1.2,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300)),),
+                          Padding(padding: EdgeInsets.only(top: 2.0)),
+                          Text("16 k/h ",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.lato(
+                                textStyle: TextStyle(
+                                    fontSize: 26.0,
+                                    height: 1.2,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700)),)
+                        ],
+                      ),
+                    ),
                   ],
                 )),
-          ]), // This trailing comma makes auto-formatting nicer for build methods.
+                Padding(padding: EdgeInsets.only(top: 24.0)),
+                Text("Opady 0,1 mm/12h",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.lato(
+                      textStyle: TextStyle(
+                          fontSize: 14.0,
+                          height: 1.2,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400)),),
+                Padding(padding: EdgeInsets.only(top: 68.0)),
+              ],
+            )),
+      ]), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
